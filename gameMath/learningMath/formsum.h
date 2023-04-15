4e49d0e2304e39594df4c29f81fc3a485a0f0f4a
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-04-15 15:20:44 +0100

    Changing data type
    Changing prototypes and type variables int to unsigned int

--- gameMath/learningMath/formsum.h
+++ gameMath/learningMath/formsum.h
@@ -18,20 +18,23 @@ public:
 
 public:
     QString result_Qs;
-    std::vector<int>vt_values;
-    int i{0},plays{35},n1{},n2{},sum_result{};
+    std::vector<unsigned int>vt_values;
+    unsigned int i{0},plays{44},n1{},n2{},n3{},sum_result{};
 
 
 public slots:
-    void ini_results(std::vector<int> &results_operation);
-    void sum_level_one(int sum);
-    void sum_level_two(int sum);
+    void ini_results(std::vector<unsigned int> &results_operation);
+    void sum_level_one(unsigned int sum);
+    void sum_level_two(unsigned int sum);
 
 private slots:
     void on_pushButton_3_clicked();
     void on_pushButton_clicked();
-    void on_lineEditc1_textEdited(int &arg1);
-    void on_lineEditc2_textEdited(int &arg1);
+    void on_lineEditc1_textEdited(unsigned int &arg1);
+    void on_lineEditc2_textEdited(unsigned int &arg1);
+    void on_lineEditc3_textEdited(unsigned int &arg1);
+    void on_lineEditc4_textEdited(unsigned int &arg1);
+    void on_lineEditc5_textEdited(unsigned int &arg1);
 
 private:
     Ui::formsum *ui;};
